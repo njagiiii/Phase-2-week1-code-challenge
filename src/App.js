e874223c814b8f0ce92763ef24ff05ba5f2420e2
njagiiii <grace.makena@student.moringaschool.com>
2023-07-24 11:32:19 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Form from './Form';
+import Data from './Data';
+
+function App() {
+  return (
+    <div>
+       <h1 className='heading'>Bank of flat iron</h1>
+       <Form />
+       <Data />
+       <footer>
+        @2023GraceMakena.Allrightreserved
+       </footer>
+    </div>
+  );
+}
+export default App;
